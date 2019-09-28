319540fbcb395c6db75965e2e81b990c67af409f
Dani <ds006518@ohio.edu>
2019-09-28 11:54:31 -0400

    Inital Commit
    Initial Commit

---
+++ Assets/Player/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public bool DebugMovement;
+    private int FramesCollidedWithTerrian;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (transform.GetComponent<CapsuleCollider>().enabled && //if grounded)
+        //{
+        //
+        //
+        //}
+
+        DebugWASD();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetComponent<TerrainCollider>().enabled) //other.tag == "collision tag name"
+        {
+            FramesCollidedWithTerrian++;
+        }
+    }
+
+    void DebugWASD()
+    {
+        if (DebugMovement)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+
+            }
+        }
+    }
+}
