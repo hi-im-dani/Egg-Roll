c8ebb5e8d947b38511bf1ee735909a716ebbda21
Dani <ds006518@ohio.edu>
2019-09-29 12:32:53 -0400

    Added control system, p2 may be working

--- Assets/Player/Scripts/MovementP2.cs
+++ Assets/Player/Scripts/MovementP2.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class MovementP2 : MonoBehaviour
 {
     public bool DebugMovement;
     public int Speed;
     private int FramesCollidedWithTerrian;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +17,8 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(Input.GetAxis("1X360_LStickY"));
-        print(Input.GetAxis("1X360_LStickX"));
+        print(Input.GetAxis("2X360_LStickY"));
+        print(Input.GetAxis("2X360_LStickX"));
         transform.GetComponent<Rigidbody>().AddForce(GameObject.Find("Camera").transform.forward * Speed * -Input.GetAxis("1X360_LStickY"));
         transform.GetComponent<Rigidbody>().AddForce(GameObject.Find("Camera").transform.right * Speed * Input.GetAxis("1X360_LStickX"));
         DebugWASD();
@@ -41,20 +41,20 @@ public class Movement : MonoBehaviour
     {
         if (DebugMovement)
         {
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.UpArrow))
             {
                 transform.GetComponent<Rigidbody>().AddForce(GameObject.Find("Camera").transform.forward * Speed);
             }
-            else if (Input.GetKey(KeyCode.A))
+            else if (Input.GetKey(KeyCode.RightArrow))
             {
                 transform.GetComponent<Rigidbody>().AddForce(-GameObject.Find("Camera").transform.right * Speed);
             }
-            else if (Input.GetKey(KeyCode.S))
+            else if (Input.GetKey(KeyCode.DownArrow))
             {
                 transform.GetComponent<Rigidbody>().AddForce(-GameObject.Find("Camera").transform.forward * Speed);
 
             }
-            else if (Input.GetKey(KeyCode.D))
+            else if (Input.GetKey(KeyCode.LeftArrow))
             {
                 transform.GetComponent<Rigidbody>().AddForce(GameObject.Find("Camera").transform.right * Speed);
             }
